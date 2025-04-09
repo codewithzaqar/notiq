0de74b4fbd03cd8ec1a5faedf8de6a6950fdf972
codewithzaqar <zaqarstandoff@gmail.com>
2025-04-09 18:03:30 +0400

    rename *version

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("\n=== Notiq v0.04 ===")
+    print("\n=== Notiq v0.05 ===")
     print("1. Add new entry")
     print("2. View all entries")
     print("3. Delete entry")
